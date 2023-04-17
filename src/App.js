b6f4e5d21d2ab6da353fb141ade1a2702ad97b59
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-04-17 00:49:37 -0400

    Chore: Setup Main and Footer

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ function App() {
   return (
     <div className="App">
         <header>
-            <div className="navbar bg-yellow-400">
+            <div className="navbar bg-yellow-600">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost btn-circle">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                         </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-blue-900 font-bold text-yellow-600 rounded-box w-52">
                         <li><a>Homepage</a></li>
                         <li><a>Calender</a></li>
                         <li><a>Ministries</a></li>
@@ -37,6 +37,10 @@ function App() {
                 </div>
             </div>
         </header>
+        <main>
+
+        </main>
+        <footer></footer>
     </div>
   );
 }
