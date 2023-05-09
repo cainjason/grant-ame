c17ed1a295798ae3c559905a2d5810807140fca9
Jason Cain <118203097+cainjason@users.noreply.github.com>
2023-05-09 10:25:37 -0400

    Chore: Setup Components

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+ return (
+     <>
+        <header> Grant A.M.E. </header>
+     </>
+ );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Header from './Components/Header'
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import Main from "./Components/Main";
 
 
 function Welcome () {
     return (
         <>
+            <Header />
+            <Navbar />
             <h1> Welcome to Grant </h1>
+            <Main />
+            <Footer />
         </>
     );
 }
